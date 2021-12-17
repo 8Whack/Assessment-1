bc7143b07382d497cdf971676875f8f71294f05d
8Whack <calvinpratt99@gmail.com>
2021-12-17 09:56:09 -0700

    Done with side, vars, and arrays

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
+// Testing & Integration
 // Implementation
 // Maintain
-// Planning
-// Testing & Integration
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,9 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "The second step, where you look over the requirements and understand what the end goal of the project is."
+const design = "This third step is the beginning of actually starting to work on code. You decide what the code will look like- commands, control flow, etc."
+const testingAndIntegration = "Now for the fun part- actually testing your code to make sure that it does what you want. both the code you wrote working on its own, and how the code works with other code that's already been implemented."
+const implementation = "this is where your code goes live, out of your testing environment and into the real world."
+const maintain = "Last but not least, maintenance is all about making sure the code continues to work after it goes live. Fixing bugs, rebuilding after crashes, things like that."
+
